c81d240712827101bf976e4bf34e644a55a71dd2
ifiok1equere <equereifiok1@gmail.com>
2023-11-08 18:05:08 +0100

    Refactor code for task 3

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 
 '''This module is the blue print for object creation
 '''
-from datetime import datetime, date
+from datetime import datetime
 import uuid
 
 
@@ -12,21 +12,11 @@ class BaseModel:
     def __init__(self, *rgs, **kwargs):
         '''This method creates an instance
         of the BaseModel Class'''
-        if kwargs:
-            del kwargs["__class__"]
-            date_str_format = "%Y-%m-%dT%H:%M:%S.%f"
-            kwargs["created_at"] = datetime.strptime(
-                    kwargs["created_at"], date_str_format
-                    )
-            kwargs["updated_at"] = datetime.strptime(
-                    kwargs["updated_at"], date_str_format
-                    )
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
 
     def __str__(self):
         '''This is the user representation
@@ -34,7 +24,7 @@ class BaseModel:
 
         obj_class_name = self.__class__.__name__
         obj_dict_rep = self.__dict__
-        return ("[{}] ({}) {}".format(obj_class_name, self.id, obj_dict_rep))
+        return ("[{:s}] ({:s}) {}".format(obj_class_name, self.id, obj_dict_rep))
 
     def save(self):
         '''Updates public instance attributes
@@ -47,9 +37,11 @@ class BaseModel:
         keys/values of __dict__ of the instance'''
 
         obj_dict = self.__dict__
+        
         obj_dict["__class__"] = self.__class__.__name__
-        created_to_str = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        obj_dict["created_at"] = created_to_str
-        updated_to_str = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        obj_dict["updated_at"] = updated_to_str
+        
+        
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+
         return obj_dict
