e0a0d206b735966d81251c9bf635d22058f27856
ifiok1equere <equereifiok1@gmail.com>
2023-11-08 18:13:51 +0100

    Refactor code a second time  for task 3

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,14 @@ import uuid
 class BaseModel:
     '''This is the model class blueprint
     '''
-    def __init__(self, *rgs, **kwargs):
+
+    def __init__(self, id=None, created_at=None, updated_at=None):
         '''This method creates an instance
         of the BaseModel Class'''
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        
 
     def __str__(self):
         '''This is the user representation
@@ -37,10 +37,7 @@ class BaseModel:
         keys/values of __dict__ of the instance'''
 
         obj_dict = self.__dict__
-        
         obj_dict["__class__"] = self.__class__.__name__
-        
-        
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
 
